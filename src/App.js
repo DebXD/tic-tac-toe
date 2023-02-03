a9693ba05770ba894d22d90684027e82ad217883
DebiprasadXD <debiprasaddaskdv@gmail.com>
2023-02-03 12:16:03 +0530

    fix import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useState} from 'react';
-import calculateWinner from "./test";
+import calculateWinner from "./winnerClac";
 
 function Square(prop) {
   return (
